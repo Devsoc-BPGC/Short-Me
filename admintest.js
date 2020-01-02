39993d7eada69b7159fa3c504039a038490a5c11
nipun agarwal <agarwalnipun12@gmail.com>
2020-01-03 02:23:53 +0530

    commit

--- admintest.js
+++ admintest.js
@@ -5,11 +5,12 @@ const User = require('../models/User');
 
 
 //This below commented code is to be run to store admin
-/*  let user = new User({name: '@Admin', email: 'Admin@123', password: '1234567', urls:[]});
+/*let user = new User({name: '@Admin', email: 'Admin@123', password: '1234567', urls:[]});
 try {
     user.save();
     console.log('Saved!');
 } catch (err) {
     console.log(err);
 }  
+
 */
